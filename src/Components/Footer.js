cefe5ba15ef2a0b40c3c3fab49b65767ee11eeab
Aaryan <aaryanporwal2233@gmail.com>
2020-11-27 05:07:44 +0530

    Frontend almost done! 🚀

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div>
+            <a href="mailto:aaryanporwal2233@gmail.com">
+                <footer className='Footer'>SUBMIT</footer>
+            </a>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Components/WindowButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WindowButton() {
+    return (
+        <div className="mylovelydiv">
+            <a className='windowButton' href="https://www.aaryanporwal.me">Open a new window somewhere in the world &rarr;</a>
+        </div>
+    )
+}
+
+export default WindowButton
